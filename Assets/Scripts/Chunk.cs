d5f40ba52a6dee45b64aa6b058d9a14c82515ca4
Filip Kaszczyński <graferubp@gmail.com>
2020-05-30 19:14:40 +0200

    Rare Materials
    Generating rare materials under the ground (carbon, diamonds etc) using Perlin Noise 3D

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -32,21 +32,21 @@ public class Chunk
                     float worldX = x + chunkObject.transform.position.x;
                     float worldY = y + chunkObject.transform.position.y;
                     float worldZ = z + chunkObject.transform.position.z;
-                    float caveProbability = ChunkUtils.CalculateCaveProbability(worldX, worldY, worldZ);
+                    float typeProbability = ChunkUtils.CalculateBlockProbability(worldX, worldY, worldZ);
                     int generated1stLayerY = (int)ChunkUtils.Generate1stLayerHeight(worldX, worldZ);
                     int generated2ndLayerY = (int)ChunkUtils.Generate2ndLayerHeight(worldX, worldZ, generated1stLayerY);
 
                     if (worldY == generated1stLayerY)
                         chunkBlocks[x, y, z] = new Block(World.blockTypes[BlockType.Type.GRASS], this, new Vector3(x, y, z));
-                    else if (caveProbability > 0.5f && worldY < generated1stLayerY - 5)
+                    else if (typeProbability > 0.5f && worldY < generated1stLayerY - 5)
                         chunkBlocks[x, y, z] = new Block(World.blockTypes[BlockType.Type.AIR], this, new Vector3(x, y, z));
                     else if (worldY < generated2ndLayerY)
                     {
-                        if (caveProbability < 0.1f)
+                        if (typeProbability < 0.1f)
                         {
                             chunkBlocks[x, y, z] = new Block(World.blockTypes[BlockType.Type.DIAMOND], this, new Vector3(x, y, z));
                         }
-                        else if (caveProbability < 0.25f)
+                        else if (typeProbability < 0.25f)
                         {
                             chunkBlocks[x, y, z] = new Block(World.blockTypes[BlockType.Type.CARBON], this, new Vector3(x, y, z));
                         }
